d1393b6cbe46d51b90a2e0687b9a08aa9c45d179
Micaiah Skolnick <mskolnickdev@gmail.com>
2023-09-20 16:17:51 -0400

    fix: better determine if using default template

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,11 @@
+# 0.2.1
+
+- Better determine full paths to templates relative paths are passed in
+
 # 0.2.0
 
-- fix publishing
+- Fix publishing
 
 # 0.1.0
 
-- initial release
+- Initial release
--- lib/src/template_manager.dart
+++ lib/src/template_manager.dart
@@ -3,6 +3,7 @@ import 'package:path/path.dart' as p;
 
 class TemplateManager {
   final String filePath;
+  final bool isBundledTemplate;
   String? _unalteredTemplate;
   String get unalteredTemplate {
     if (_unalteredTemplate != null) {
@@ -10,20 +11,21 @@ class TemplateManager {
     }
     var path = '';
     // If the file path is absolute, we assume the user passed in a path override
-    if (p.isAbsolute(filePath)) {
-      path = filePath;
-    } else {
-      // Here we assume the path to the template is the default
-      // So we do some manipulation to resolve the uri to the asset folder
+    if (isBundledTemplate) {
+      // If we're using the bundled template, we need to do
+      // some file path manipulation to resolve the uri to the asset folder
       // inside the changelog_bubbler package
       final basePackagePath = File(Platform.script.toFilePath()).parent.parent;
       path = p.join(basePackagePath.path, 'template', filePath);
+    } else {
+      path = filePath;
     }
     final templateFile = File(path);
     return _unalteredTemplate ??= templateFile.readAsStringSync();
   }
 
-  TemplateManager(this.filePath, {String? templateForTesting})
+  TemplateManager(this.filePath,
+      {required this.isBundledTemplate, String? templateForTesting})
       : _unalteredTemplate = templateForTesting;
 
   String replaceAll(Map<String, String> replacements) {
